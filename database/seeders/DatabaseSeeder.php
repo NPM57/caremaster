8d0229b2127d932014d7146c0c807a1c06ab9862
NPM57 <phuongmy57@gmail.com>
2023-05-28 20:35:52 +1000

    update DatabaseSeeder.php to call company and employee seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,17 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Admin',
-        //     'email' => 'admin@admin.com',
-        //     'password' => 'password',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Admin',
+             'email' => 'admin@admin.com',
+             'password' => 'password',
+         ]);
 
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+
+        $this->call([
+            CompanySeeder::class,
+            EmployeeSeeder::class,
+        ]);
     }
 }
