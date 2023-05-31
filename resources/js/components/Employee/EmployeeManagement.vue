36daa640edb59de9cdaee89250b6b7b31acb9ea9
NPM57 <phuongmy57@gmail.com>
2023-06-01 02:12:21 +1000

    bring EmployeeManagement.vue into its own directory

---
+++ resources/js/components/Employee/EmployeeManagement.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Employee Management Page
+    </div>
+</template>
+
+
+<script>
+import axios from 'axios';
+
+export default {
+    components: {
+    },
+    data() {
+        return {
+        }
+    },
+    created() {
+    },
+
+    methods: {
+    },
+}
+</script>
